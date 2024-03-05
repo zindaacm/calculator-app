17f21cf006b5f0c05857701aa244656b7104dfb7
zindaacm <zindaachmad@students.amikom.ac.id>
2024-03-05 12:08:34 +0700

    Melengkapi fungsi Main (revisi)

--- Calculator.cs
+++ Calculator.cs
@@ -6,7 +6,7 @@ class Calculator
 		int b = 6;
 		
 		Console.WriteLine(Hasil Penambahan: {0} + {1} = {2}, a, b, Penambahan(a, b));
-		Console.WriteLine(Hasil Penambahan: {0} - {1} = {2}, a, b, Pengurangan(a, b));
+		Console.WriteLine(Hasil Pengurangan: {0} - {1} = {2}, a, b, Pengurangan(a, b));
 		
 		Console.WriteLine("/nTekan sembarang key untuk keluar");
 		Console.ReadKey();
